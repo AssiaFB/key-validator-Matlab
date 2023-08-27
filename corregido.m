cc650ee671a11c5d304bd02d1a28794221e138e8
Assia_Second_account <143298997+AssiaFB@users.noreply.github.com>
2023-08-26 21:36:48 -0500

    Version 1.2
    Version 1.2

--- corregido.m
+++ corregido.m
@@ -2,29 +2,32 @@ clc
 clear
 disp('By Jesús Bautista')
 clave=abs(input('Ingresa tu clave de 3 digitos: \n'));
-cont=0;
+cont=1;
 p=0;
 %lo hice a mi manera
-pause(1.5)
+pause(1)
 clc
 while true
-    cont=cont+1;
-    if clave==123
+    if cont<=3
+        if clave==123
         clc
         pause(1)
         disp('¡Contraseña correcta!')
         disp('By Jesús Bautista')
         break
-    elseif isempty(clave)
+        elseif isempty(clave)
         clc
+        cont=cont+1;
         pause(1)
         disp('Ingresa nuevamente tu clave de 3 digitos')
         clave=abs(input('Ingresa tu clave de 3 digitos: \n'));
-    else
+        else
         clc
+        cont=cont+1;
         pause(1)
         disp('Ingresa nuevamente tu clave de 3 digitos')
         clave=abs(input('Ingresa tu clave de 3 digitos: \n'));
+        end
     end
     if cont==3
         if clave==123
@@ -33,16 +36,18 @@ while true
         disp('¡Contraseña correcta!')
         disp('By Jesús Bautista')
         break
-        end
+        else
         pause(1)
-        disp('Intenta mañana')
+        clc
+        disp('Intenta más tarde')
         p=1;
         break
+        end
     end
 end
 if p==0
     fprintf('Ingresaste tu clave correcta después de %d intentos',cont)
-    pause(2)
+    pause(3)
     clc
     disp('BIENVENIDO SEÑOR')
     carga=waitbar(0,'INICIANDO...(By Jesus Bautista)');
